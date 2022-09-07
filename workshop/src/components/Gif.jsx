eb52c85cce265b0082a5b607c2c0494a3fcefb57
Carla Willmes <willmes.carla@gmail.com>
2022-09-07 21:01:03 +0900

    built react app

---
+++ workshop/src/components/Gif.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Gif = (props) => {
+  const { gifId, changeSelectGif } = props;
+  const url = `https://media.giphy.com/media/${gifId}/giphy.gif`;
+  const handleClick = () => {
+    changeSelectGif(gifId);
+  };
+
+  return (
+    <img src={url} alt="gif" className="gif" onClick={handleClick} />
+  );
+};
+
+export default Gif;
